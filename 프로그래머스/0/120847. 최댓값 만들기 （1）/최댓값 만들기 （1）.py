b9d53aa4b6380df328a052e552d6fe241a61b413
KimSeongMi <104122239+hisungmi@users.noreply.github.com>
2024-02-26 16:05:18 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = 0
+    for i in range(len(numbers)):
+        for j in range(i+1,len(numbers)):
+            num = numbers[i] * numbers[j]
+            print(num)
+            if num > answer:
+                answer = num
+    return answer
