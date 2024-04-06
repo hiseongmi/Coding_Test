0a81272899b9fbeef5553e25b8e0c46f8153e4c4
KimSeongMi <104122239+hisungmi@users.noreply.github.com>
2024-04-06 16:15:12 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+while(True):
+    s = input().rstrip()
+    stack =[]
+    if s == ".":
+        break
+    for i in s:
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(i)
+                break
+        elif i == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(i)
+                break
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
