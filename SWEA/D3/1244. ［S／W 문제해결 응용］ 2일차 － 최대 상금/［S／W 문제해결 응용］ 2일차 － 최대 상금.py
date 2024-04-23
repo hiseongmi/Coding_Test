be3ba07ada3bb5fde5ef1dd72384aab22beda834
KimSeongMi <104122239+hisungmi@users.noreply.github.com>
2024-04-24 00:16:51 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 136 ms, Memory: 48,736 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,17 +1,18 @@
 t = int(input())
 for tc in range(1, t+1):
-    s, n = input().split()
-    n = int(n)
-    num = {s}
-    check = set()
-    for _ in range(n):
-        while num:
-            v = num.pop()
-            v = list(v)
-            for i in range(len(s)):
-                for j in range(i+1, len(s)):
-                    v[i], v[j] = v[j], v[i]
-                    check.add(''.join(v))
-                    v[i], v[j] = v[j], v[i]
-        num, check = check, num
-    print(f"#{tc} {max(map(int,num))}")
+    S, N = input().split()
+    N = int(N)
+    arr = {S}
+    arr_n = set()
+    for _ in range(N):
+        #횟수만큼 배열 조합 을 arr_n에 넣음 조합이 다 들어갈때까지
+        while arr:
+            a = arr.pop()
+            a = list(a)
+            for i in range(len(S)):
+                for j in range(i+1, len(S)):
+                    a[i], a[j] = a[j], a[i]
+                    arr_n.add(''.join(a))
+                    a[i], a[j] = a[j], a[i]
+        arr, arr_n = arr_n, arr
+    print(f"#{tc}", max(map(int, arr)))
