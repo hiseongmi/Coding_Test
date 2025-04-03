41056e55b801c3ca98cbf6ee2b8a393a853e958a
KimSeongMi <104122239+hiseongmi@users.noreply.github.com>
2025-04-03 16:45:20 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 224 ms, Memory: 55968 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,20 +1,18 @@
 import sys
 input = sys.stdin.readline
-n,  m = map(int, input().split())
-d = {}
-e = {}
-for i in range(n):
-    p = input().strip() #strip 안하면 /n 이붙음
-    d[str(i+1)] = p # 문자출력 d={"1":"asdf"}
-    e[p] = i+1 # e={"asdf":"3"}
-r = []
-for i in range(m):
-    q = input().strip() #공백제거
-    if q in e:
-        r.append(e[q])
+
+dogam = {}
+n, m = map(int, input().split())
+for i in range(1, n+1):
+    name = input().strip()
+    dogam[i] = name
+    dogam[name] = i
+for j in range(m):
+    v = input().strip()
+    
+    if v.isdigit():
+        print(dogam[int(v)])
     else:
-        r.append(d[q])
-
-for i in r:
-    print(i)
-
+        print(dogam[v])
+        
+    
