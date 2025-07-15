8e5203e13f7e0afbb4fc2b53a2b10b73881da6cf
KimSeongMi <104122239+hiseongmi@users.noreply.github.com>
2025-07-15 20:42:10 +0900

    [Silver IV] Title: 대피소, Time: 36 ms, Memory: 32412 KB, Score: 7 point -BaekjoonHub

---
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -0,0 +1,26 @@
+from itertools import combinations
+
+n, k = map(int, input().split())
+arr = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    arr.append((x,y))
+
+def dist(x,y,x2,y2):
+    return int(abs(x - x2) + abs(y - y2))
+
+
+ans = 9999
+for no in combinations(range(n), k):
+    max_d = 0
+    for i in range(n):
+        x, y = arr[i]
+        d = 9999
+        for j in no:
+            x2, y2 = arr[j]
+            d = min(d, dist(x, y, x2, y2))
+        max_d = max(max_d, d)
+    ans = min(ans, max_d)
+
+
+print(ans)
