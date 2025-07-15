ffa32098d29cc09574f0ff26431995135dce138d
KimSeongMi <104122239+hiseongmi@users.noreply.github.com>
2025-07-15 20:45:26 +0900

    [Silver IV] Title: 대피소, Time: 1756 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -10,12 +10,12 @@ def dist(x,y,x2,y2):
     return int(abs(x - x2) + abs(y - y2))
 
 
-ans = 9999
+ans = float("INF")
 for no in combinations(range(n), k):
     max_d = 0
     for i in range(n):
         x, y = arr[i]
-        d = 9999
+        d = float("INF")
         for j in no:
             x2, y2 = arr[j]
             d = min(d, dist(x, y, x2, y2))
